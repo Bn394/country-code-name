6e2bf98fa4e9c5317ac4b9fa7fca614941555c56
Bramhanand Garg <124577049+Bn394@users.noreply.github.com>
2023-12-07 15:15:23 +0530

    initial commit

---
+++ constant.js
@@ -0,0 +1,998 @@
+export const allTimeZone = [
+  {
+    Name: "Afghanistan",
+    MobileCode: "+93",
+  },
+  {
+    Name: "Åland Islands",
+    MobileCode: "+358-18",
+  },
+  {
+    Name: "Albania",
+    MobileCode: "+355",
+  },
+  {
+    Name: "Algeria",
+    MobileCode: "+213",
+  },
+  {
+    Name: "American Samoa",
+    MobileCode: "+1-684",
+  },
+  {
+    Name: "Andorra",
+    MobileCode: "+376",
+  },
+  {
+    Name: "Angola",
+    MobileCode: "+244",
+  },
+  {
+    Name: "Anguilla",
+    MobileCode: "+1-264",
+  },
+  {
+    Name: "Antarctica",
+    MobileCode: "+",
+  },
+  {
+    Name: "Antigua and Barbuda",
+    MobileCode: "+1-268",
+  },
+  {
+    Name: "Argentina",
+    MobileCode: "+54",
+  },
+  {
+    Name: "Armenia",
+    MobileCode: "+374",
+  },
+  {
+    Name: "Aruba",
+    MobileCode: "+297",
+  },
+  {
+    Name: "Australia",
+    MobileCode: "+61",
+  },
+  {
+    Name: "Austria",
+    MobileCode: "+43",
+  },
+  {
+    Name: "Azerbaijan",
+    MobileCode: "+994",
+  },
+  {
+    Name: "Bahamas, The",
+    MobileCode: "+1-242",
+  },
+  {
+    Name: "Bahrain",
+    MobileCode: "+973",
+  },
+  {
+    Name: "Bangladesh",
+    MobileCode: "+880",
+  },
+  {
+    Name: "Barbados",
+    MobileCode: "+1-246",
+  },
+  {
+    Name: "Belarus",
+    MobileCode: "+375",
+  },
+  {
+    Name: "Belgium",
+    MobileCode: "+32",
+  },
+  {
+    Name: "Belize",
+    MobileCode: "+501",
+  },
+  {
+    Name: "Benin",
+    MobileCode: "+229",
+  },
+  {
+    Name: "Bermuda",
+    MobileCode: "+1-441",
+  },
+  {
+    Name: "Bhutan",
+    MobileCode: "+975",
+  },
+  {
+    Name: "Bolivarian Republic of Venezuela",
+    MobileCode: "+58",
+  },
+  {
+    Name: "Bolivia",
+    MobileCode: "+591",
+  },
+  {
+    Name: "Bonaire, Sint Eustatius and Saba",
+    MobileCode: "+599",
+  },
+  {
+    Name: "Bosnia and Herzegovina",
+    MobileCode: "+387",
+  },
+  {
+    Name: "Botswana",
+    MobileCode: "+267",
+  },
+  {
+    Name: "Bouvet Island",
+    MobileCode: "+",
+  },
+  {
+    Name: "Brazil",
+    MobileCode: "+55",
+  },
+  {
+    Name: "British Indian Ocean Territory",
+    MobileCode: "+246",
+  },
+  {
+    Name: "Brunei",
+    MobileCode: "+673",
+  },
+  {
+    Name: "Bulgaria",
+    MobileCode: "+359",
+  },
+  {
+    Name: "Burkina Faso",
+    MobileCode: "+226",
+  },
+  {
+    Name: "Burundi",
+    MobileCode: "+257",
+  },
+  {
+    Name: "Cabo Verde",
+    MobileCode: "+238",
+  },
+  {
+    Name: "Cambodia",
+    MobileCode: "+855",
+  },
+  {
+    Name: "Cameroon",
+    MobileCode: "+237",
+  },
+  {
+    Name: "Canada",
+    MobileCode: "+1",
+  },
+  {
+    Name: "Cayman Islands",
+    MobileCode: "+1-345",
+  },
+  {
+    Name: "Central African Republic",
+    MobileCode: "+236",
+  },
+  {
+    Name: "Chad",
+    MobileCode: "+235",
+  },
+  {
+    Name: "Chile",
+    MobileCode: "+56",
+  },
+  {
+    Name: "China",
+    MobileCode: "+86",
+  },
+  {
+    Name: "Christmas Island",
+    MobileCode: "+61",
+  },
+  {
+    Name: "Cocos (Keeling) Islands",
+    MobileCode: "+61",
+  },
+  {
+    Name: "Colombia",
+    MobileCode: "+57",
+  },
+  {
+    Name: "Comoros",
+    MobileCode: "+269",
+  },
+  {
+    Name: "Congo",
+    MobileCode: "+242",
+  },
+  {
+    Name: "Congo (DRC)",
+    MobileCode: "+243",
+  },
+  {
+    Name: "Cook Islands",
+    MobileCode: "+682",
+  },
+  {
+    Name: "Costa Rica",
+    MobileCode: "+506",
+  },
+  {
+    Name: "Cote d'Ivoire",
+    MobileCode: "+225",
+  },
+  {
+    Name: "Croatia",
+    MobileCode: "+385",
+  },
+  {
+    Name: "Cuba",
+    MobileCode: "+53",
+  },
+  {
+    Name: "Curacao",
+    MobileCode: "+599",
+  },
+  {
+    Name: "Cyprus",
+    MobileCode: "+357",
+  },
+  {
+    Name: "Czech Republic",
+    MobileCode: "+420",
+  },
+  {
+    Name: "Denmark",
+    MobileCode: "+45",
+  },
+  {
+    Name: "Djibouti",
+    MobileCode: "+253",
+  },
+  {
+    Name: "Dominica",
+    MobileCode: "+1-767",
+  },
+  {
+    Name: "Dominican Republic",
+    MobileCode: "+1-809",
+  },
+  {
+    Name: "East Timor",
+    MobileCode: "+670",
+  },
+  {
+    Name: "Ecuador",
+    MobileCode: "+593",
+  },
+  {
+    Name: "Egypt",
+    MobileCode: "+20",
+  },
+  {
+    Name: "El Salvador",
+    MobileCode: "+503",
+  },
+  {
+    Name: "Equatorial Guinea",
+    MobileCode: "+240",
+  },
+  {
+    Name: "Eritrea",
+    MobileCode: "+291",
+  },
+  {
+    Name: "Estonia",
+    MobileCode: "+372",
+  },
+  {
+    Name: "Ethiopia",
+    MobileCode: "+251",
+  },
+  {
+    Name: "Falkland Islands (Islas Malvinas)",
+    MobileCode: "+500",
+  },
+  {
+    Name: "Faroe Islands",
+    MobileCode: "+298",
+  },
+  {
+    Name: "Fiji Islands",
+    MobileCode: "+679",
+  },
+  {
+    Name: "Finland",
+    MobileCode: "+358",
+  },
+  {
+    Name: "France",
+    MobileCode: "+33",
+  },
+  {
+    Name: "French Guiana",
+    MobileCode: "+594",
+  },
+  {
+    Name: "French Polynesia",
+    MobileCode: "+689",
+  },
+  {
+    Name: "French Southern and Antarctic Lands",
+    MobileCode: "+262",
+  },
+  {
+    Name: "Gabon",
+    MobileCode: "+241",
+  },
+  {
+    Name: "Gambia",
+    MobileCode: "+220",
+  },
+  {
+    Name: "Georgia",
+    MobileCode: "+995",
+  },
+  {
+    Name: "Germany",
+    MobileCode: "+49",
+  },
+  {
+    Name: "Ghana",
+    MobileCode: "+233",
+  },
+  {
+    Name: "Gibraltar",
+    MobileCode: "+350",
+  },
+  {
+    Name: "Greece",
+    MobileCode: "+30",
+  },
+  {
+    Name: "Greenland",
+    MobileCode: "+299",
+  },
+  {
+    Name: "Grenada",
+    MobileCode: "+1-473",
+  },
+  {
+    Name: "Guadeloupe",
+    MobileCode: "+590",
+  },
+  {
+    Name: "Guam",
+    MobileCode: "+1-671",
+  },
+  {
+    Name: "Guatemala",
+    MobileCode: "+502",
+  },
+  {
+    Name: "Guernsey",
+    MobileCode: "+44",
+  },
+  {
+    Name: "Guinea",
+    MobileCode: "+224",
+  },
+  {
+    Name: "Guinea-Bissau",
+    MobileCode: "+245",
+  },
+  {
+    Name: "Guyana",
+    MobileCode: "+592",
+  },
+  {
+    Name: "Haiti",
+    MobileCode: "+509",
+  },
+  {
+    Name: "Heard Island and McDonald Islands",
+    MobileCode: "+",
+  },
+  {
+    Name: "Honduras",
+    MobileCode: "+504",
+  },
+  {
+    Name: "Hong Kong",
+    MobileCode: "+852",
+  },
+  {
+    Name: "Hungary",
+    MobileCode: "+36",
+  },
+  {
+    Name: "Iceland",
+    MobileCode: "+354",
+  },
+  {
+    Name: "India",
+    MobileCode: "+91",
+  },
+  {
+    Name: "Indonesia",
+    MobileCode: "+62",
+  },
+  {
+    Name: "Iran",
+    MobileCode: "+98",
+  },
+  {
+    Name: "Iraq",
+    MobileCode: "+964",
+  },
+  {
+    Name: "Ireland",
+    MobileCode: "+353",
+  },
+  {
+    Name: "Isle of Man",
+    MobileCode: "+44",
+  },
+  {
+    Name: "Israel",
+    MobileCode: "+972",
+  },
+  {
+    Name: "Italy",
+    MobileCode: "+39",
+  },
+  {
+    Name: "Jamaica",
+    MobileCode: "+1-876",
+  },
+  {
+    Name: "Japan",
+    MobileCode: "+81",
+  },
+  {
+    Name: "Jersey",
+    MobileCode: "+44",
+  },
+  {
+    Name: "Jordan",
+    MobileCode: "+962",
+  },
+  {
+    Name: "Kazakhstan",
+    MobileCode: "+7",
+  },
+  {
+    Name: "Kenya",
+    MobileCode: "+254",
+  },
+  {
+    Name: "Kiribati",
+    MobileCode: "+686",
+  },
+  {
+    Name: "Korea",
+    MobileCode: "+82",
+  },
+  {
+    Name: "Kosovo",
+    MobileCode: "+383",
+  },
+  {
+    Name: "Kuwait",
+    MobileCode: "+965",
+  },
+  {
+    Name: "Kyrgyzstan",
+    MobileCode: "+996",
+  },
+  {
+    Name: "Laos",
+    MobileCode: "+856",
+  },
+  {
+    Name: "Latvia",
+    MobileCode: "+371",
+  },
+  {
+    Name: "Lebanon",
+    MobileCode: "+961",
+  },
+  {
+    Name: "Lesotho",
+    MobileCode: "+266",
+  },
+  {
+    Name: "Liberia",
+    MobileCode: "+231",
+  },
+  {
+    Name: "Libya",
+    MobileCode: "+218",
+  },
+  {
+    Name: "Liechtenstein",
+    MobileCode: "+423",
+  },
+  {
+    Name: "Lithuania",
+    MobileCode: "+370",
+  },
+  {
+    Name: "Luxembourg",
+    MobileCode: "+352",
+  },
+  {
+    Name: "Macao",
+    MobileCode: "+853",
+  },
+  {
+    Name: "Macedonia",
+    MobileCode: "+389",
+  },
+  {
+    Name: "Madagascar",
+    MobileCode: "+261",
+  },
+  {
+    Name: "Malawi",
+    MobileCode: "+265",
+  },
+  {
+    Name: "Malaysia",
+    MobileCode: "+60",
+  },
+  {
+    Name: "Maldives",
+    MobileCode: "+960",
+  },
+  {
+    Name: "Mali",
+    MobileCode: "+223",
+  },
+  {
+    Name: "Malta",
+    MobileCode: "+356",
+  },
+  {
+    Name: "Marshall Islands",
+    MobileCode: "+692",
+  },
+  {
+    Name: "Martinique",
+    MobileCode: "+596",
+  },
+  {
+    Name: "Mauritania",
+    MobileCode: "+222",
+  },
+  {
+    Name: "Mauritius",
+    MobileCode: "+230",
+  },
+  {
+    Name: "Mayotte",
+    MobileCode: "+262",
+  },
+  {
+    Name: "Mexico",
+    MobileCode: "+52",
+  },
+  {
+    Name: "Micronesia",
+    MobileCode: "+691",
+  },
+  {
+    Name: "Moldova",
+    MobileCode: "+373",
+  },
+  {
+    Name: "Monaco",
+    MobileCode: "+377",
+  },
+  {
+    Name: "Mongolia",
+    MobileCode: "+976",
+  },
+  {
+    Name: "Montenegro",
+    MobileCode: "+382",
+  },
+  {
+    Name: "Montserrat",
+    MobileCode: "+1-664",
+  },
+  {
+    Name: "Morocco",
+    MobileCode: "+212",
+  },
+  {
+    Name: "Mozambique",
+    MobileCode: "+258",
+  },
+  {
+    Name: "Myanmar",
+    MobileCode: "+95",
+  },
+  {
+    Name: "Namibia",
+    MobileCode: "+264",
+  },
+  {
+    Name: "Nauru",
+    MobileCode: "+674",
+  },
+  {
+    Name: "Nepal",
+    MobileCode: "+977",
+  },
+  {
+    Name: "Netherlands",
+    MobileCode: "+31",
+  },
+  {
+    Name: "New Caledonia",
+    MobileCode: "+687",
+  },
+  {
+    Name: "New Zealand",
+    MobileCode: "+64",
+  },
+  {
+    Name: "Nicaragua",
+    MobileCode: "+505",
+  },
+  {
+    Name: "Niger",
+    MobileCode: "+227",
+  },
+  {
+    Name: "Nigeria",
+    MobileCode: "+234",
+  },
+  {
+    Name: "Niue",
+    MobileCode: "+683",
+  },
+  {
+    Name: "Norfolk Island",
+    MobileCode: "+672",
+  },
+  {
+    Name: "North Korea",
+    MobileCode: "+850",
+  },
+  {
+    Name: "Northern Mariana Islands",
+    MobileCode: "+1-670",
+  },
+  {
+    Name: "Norway",
+    MobileCode: "+47",
+  },
+  {
+    Name: "Oman",
+    MobileCode: "+968",
+  },
+  {
+    Name: "Pakistan",
+    MobileCode: "+92",
+  },
+  {
+    Name: "Palau",
+    MobileCode: "+680",
+  },
+  {
+    Name: "Palestine",
+    MobileCode: "+970",
+  },
+  {
+    Name: "Panama",
+    MobileCode: "+507",
+  },
+  {
+    Name: "Papua New Guinea",
+    MobileCode: "+675",
+  },
+  {
+    Name: "Paraguay",
+    MobileCode: "+595",
+  },
+  {
+    Name: "Peru",
+    MobileCode: "+51",
+  },
+  {
+    Name: "Philippines",
+    MobileCode: "+63",
+  },
+  {
+    Name: "Pitcairn Islands",
+    MobileCode: "+870",
+  },
+  {
+    Name: "Poland",
+    MobileCode: "+48",
+  },
+  {
+    Name: "Portugal",
+    MobileCode: "+351",
+  },
+  {
+    Name: "Puerto Rico",
+    MobileCode: "+1-787",
+  },
+  {
+    Name: "Qatar",
+    MobileCode: "+974",
+  },
+  {
+    Name: "Réunion",
+    MobileCode: "+262",
+  },
+  {
+    Name: "Romania",
+    MobileCode: "+40",
+  },
+  {
+    Name: "Russia",
+    MobileCode: "+7",
+  },
+  {
+    Name: "Rwanda",
+    MobileCode: "+250",
+  },
+  {
+    Name: "Saint Barthélemy",
+    MobileCode: "+590",
+  },
+  {
+    Name: "Saint Helena",
+    MobileCode: "+290",
+  },
+  {
+    Name: "Saint Kitts and Nevis",
+    MobileCode: "+1-869",
+  },
+  {
+    Name: "Saint Lucia",
+    MobileCode: "+1-758",
+  },
+  {
+    Name: "Saint Martin",
+    MobileCode: "+590",
+  },
+  {
+    Name: "Saint Pierre and Miquelon",
+    MobileCode: "+508",
+  },
+  {
+    Name: "Saint Vincent and the Grenadines",
+    MobileCode: "+1-784",
+  },
+  {
+    Name: "Samoa",
+    MobileCode: "+685",
+  },
+  {
+    Name: "San Marino",
+    MobileCode: "+378",
+  },
+  {
+    Name: "São Tomé and Príncipe",
+    MobileCode: "+239",
+  },
+  {
+    Name: "Saudi Arabia",
+    MobileCode: "+966",
+  },
+  {
+    Name: "Senegal",
+    MobileCode: "+221",
+  },
+  {
+    Name: "Serbia",
+    MobileCode: "+381",
+  },
+  {
+    Name: "Seychelles",
+    MobileCode: "+248",
+  },
+  {
+    Name: "Sierra Leone",
+    MobileCode: "+232",
+  },
+  {
+    Name: "Singapore",
+    MobileCode: "+65",
+  },
+  {
+    Name: "Sint Maarten",
+    MobileCode: "+599",
+  },
+  {
+    Name: "Slovakia",
+    MobileCode: "+421",
+  },
+  {
+    Name: "Slovenia",
+    MobileCode: "+386",
+  },
+  {
+    Name: "Solomon Islands",
+    MobileCode: "+677",
+  },
+  {
+    Name: "Somalia",
+    MobileCode: "+252",
+  },
+  {
+    Name: "South Africa",
+    MobileCode: "+27",
+  },
+  {
+    Name: "South Georgia and the South Sandwich Islands",
+    MobileCode: "+500",
+  },
+  {
+    Name: "South Sudan",
+    MobileCode: "+211",
+  },
+  {
+    Name: "Spain",
+    MobileCode: "+34",
+  },
+  {
+    Name: "Sri Lanka",
+    MobileCode: "+94",
+  },
+  {
+    Name: "Sudan",
+    MobileCode: "+249",
+  },
+  {
+    Name: "Suriname",
+    MobileCode: "+597",
+  },
+  {
+    Name: "Svalbard and Jan Mayen",
+    MobileCode: "+47",
+  },
+  {
+    Name: "Swaziland",
+    MobileCode: "+268",
+  },
+  {
+    Name: "Sweden",
+    MobileCode: "+46",
+  },
+  {
+    Name: "Switzerland",
+    MobileCode: "+41",
+  },
+  {
+    Name: "Syria",
+    MobileCode: "+963",
+  },
+  {
+    Name: "Taiwan",
+    MobileCode: "+886",
+  },
+  {
+    Name: "Tajikistan",
+    MobileCode: "+992",
+  },
+  {
+    Name: "Tanzania",
+    MobileCode: "+255",
+  },
+  {
+    Name: "Thailand",
+    MobileCode: "+66",
+  },
+  {
+    Name: "Togo",
+    MobileCode: "+228",
+  },
+  {
+    Name: "Tokelau",
+    MobileCode: "+690",
+  },
+  {
+    Name: "Tonga",
+    MobileCode: "+676",
+  },
+  {
+    Name: "Trinidad and Tobago",
+    MobileCode: "+1-868",
+  },
+  {
+    Name: "Tunisia",
+    MobileCode: "+216",
+  },
+  {
+    Name: "Turkey",
+    MobileCode: "+90",
+  },
+  {
+    Name: "Turkmenistan",
+    MobileCode: "+993",
+  },
+  {
+    Name: "Turks and Caicos Islands",
+    MobileCode: "+1-649",
+  },
+  {
+    Name: "Tuvalu",
+    MobileCode: "+688",
+  },
+  {
+    Name: "Uganda",
+    MobileCode: "+256",
+  },
+  {
+    Name: "Ukraine",
+    MobileCode: "+380",
+  },
+  {
+    Name: "United Arab Emirates",
+    MobileCode: "+971",
+  },
+  {
+    Name: "United Kingdom",
+    MobileCode: "+44",
+  },
+  {
+    Name: "United States",
+    MobileCode: "+1",
+  },
+  {
+    Name: "United States Minor Outlying Islands",
+    MobileCode: "+1",
+  },
+  {
+    Name: "Uruguay",
+    MobileCode: "+598",
+  },
+  {
+    Name: "Uzbekistan",
+    MobileCode: "+998",
+  },
+  {
+    Name: "Vanuatu",
+    MobileCode: "+678",
+  },
+  {
+    Name: "Vatican City",
+    MobileCode: "+39",
+  },
+  {
+    Name: "Vietnam",
+    MobileCode: "+84",
+  },
+  {
+    Name: "Virgin Islands",
+    MobileCode: "+1-340",
+  },
+  {
+    Name: "Wallis and Futuna",
+    MobileCode: "+681",
+  },
+  {
+    Name: "Western Sahara",
+    MobileCode: "+212",
+  },
+  {
+    Name: "Yemen",
+    MobileCode: "+967",
+  },
+  {
+    Name: "Zambia",
+    MobileCode: "+260",
+  },
+  {
+    Name: "Zimbabwe",
+    MobileCode: "+263",
+  },
+];
